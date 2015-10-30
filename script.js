b066be626a2e3af12f272f0a2e1bb3eefa751e8d
Griffin Hammer <grifhammer@gmail.com>
2015-10-30 09:54:06 -0400

    Running Clock with Red Coloring
    Clock runs and will turn red for 1 second at the minute change. Waiting
    to confirm that clock changes blue for a minute every hour.

--- script.js
+++ script.js
@@ -1,11 +1,40 @@
 $(document).ready(function(){
+	function fixZero(i){
+		if(i < 10){
+			i = "0" + i;
+		}
+		return i;
+	}
+	function updateTime(){
+		currTime = new Date();
+		h = fixZero(currTime.getHours());
+		m = fixZero(currTime.getMinutes());
+		s = fixZero(currTime.getSeconds());
+		$('.hour').text(h)
+		$('.minute').text(m)
+		$('.second').text(s)
+		if(m == 0 && s ==0){
+			addColor('blue',60000);
+		}
+		if(s == 0){
+			addColor('red',1000);
+		}
+		
+	}
 	currTime = new Date();
-	updateTime(currTime);
-
+	var runClock = setInterval(updateTime, 500);
+	function addColor(color, timeOut){
+		$('#time').addClass(color);
+		setTimeout(removeColor, timeOut, color);
+	}
+	function removeColor(color){
+		$('#time').removeClass(color);
+	}
+	$('.stop').click(function(){
+		clearInterval(runClock);
+	});
+	$('.go').click(function(){
+		runClock = setInterval(updateTime, 500);
+	});
+	
 });
-
-function updateTime(currDateObj){
-	$('.hour').text(currDateObj.getHours())
-	$('.minute').text(currDateObj.getMinutes())
-	$('.second').text(currDateObj.getSeconds())
-}
